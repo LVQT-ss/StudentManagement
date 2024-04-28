5277f3f88f2e3e9e440241c055e8d874d2f96c2b
LVQT <levietquocthinh@gmail.com>
2024-04-28 15:34:12 +0700

    added swgger in projetc resful

--- application/src/main/java/com/studentapp/application/controller/StudentController.java
+++ application/src/main/java/com/studentapp/application/controller/StudentController.java
@@ -3,10 +3,7 @@ package com.studentapp.application.controller;
 import com.studentapp.application.dto.StudentSaveDTO;
 import com.studentapp.application.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
@@ -15,7 +12,7 @@ public class StudentController {
 
     @Autowired
     private StudentService studentService;
-
+    @PostMapping("/add")
     public String saveStudent(@RequestBody StudentSaveDTO studentSaveDTO){
         String id = studentService.addStudent(studentSaveDTO);
         return id;
